fcb5d4ee5e6d8310c71cf158804b73954c079a4f
SimonC <SimonC@host.docker.internal>
2020-01-22 16:09:38 +0800

    change campaign status

---
+++ src/main/java/com/leosys/adwords/bu4/lead10/google_ads_api/jsons/request/CampaignRequestBody.java
@@ -0,0 +1,17 @@
+package com.leosys.adwords.bu4.lead10.google_ads_api.jsons.request;
+
+public class CampaignRequestBody {
+
+	private String status;
+	
+	public CampaignRequestBody() {
+	}
+
+	public String getStatus() {
+			return status;
+	}
+
+	public void setStatus(String status) {
+			this.status = status;
+	}
+}
