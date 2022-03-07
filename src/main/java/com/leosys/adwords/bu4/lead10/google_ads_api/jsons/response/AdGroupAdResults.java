cbde6706e6f67612e31f4ae6ca2ad52f4b8ba23c
SimonC <SimonC@leosys.com>
2022-03-07 08:57:50 +0800

    disapprovedAds

---
+++ src/main/java/com/leosys/adwords/bu4/lead10/google_ads_api/jsons/response/AdGroupAdResults.java
@@ -0,0 +1,25 @@
+package com.leosys.adwords.bu4.lead10.google_ads_api.jsons.response;
+
+import java.util.List;
+import java.util.Map;
+
+import com.leosys.adwords.bu4.lead10.jsons.response.IntResult;
+
+public interface AdGroupAdResults {
+	
+	public static class DisapprovedAds extends IntResult {
+		
+			private List<Map<String, Object>> disapprovedAds;
+			
+			public DisapprovedAds() {}
+			
+			public DisapprovedAds(int result, String description, List<Map<String, Object>> disapprovedAds) {
+					super(result, description);
+					this.disapprovedAds = disapprovedAds;
+			}
+
+			public List<Map<String, Object>> getDisapprovedAds() {
+					return disapprovedAds;
+			}
+	}
+}
